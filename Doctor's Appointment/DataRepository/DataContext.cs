9567c91e3089c4af60cd04a0c008aa6f43330f24
Ibrahim-Khan88 <ebrahimkhanobak@gmail.com>
2023-06-27 22:39:32 +0600

    Project setup done

---
+++ Doctor's Appointment/DataRepository/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Doctor_s_Appointment.Models;
+
+namespace Doctor_s_Appointment.DataRepository
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
---
+++ Doctor's Appointment/Models/TestAgain.cs
@@ -0,0 +1,8 @@
+﻿namespace Doctor_s_Appointment.Models
+{
+    public class TestAgain
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+}
